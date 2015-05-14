fd7a0ab498454a195917f05392649c31b8fe37a0
hjorthjort <rikard.hjort@gmail.com>
2015-05-14 18:58:19 +0200

    Fix bug in home.js

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -1,5 +1,6 @@
-Template.channel.events({
+Template.home.events({
   'keyup textarea' : function(event, instance) {
+    console.log(event);
     if (event.keyCode == 13 && !event.shiftKey) {
       var value = instance.find('textarea').value;
       instance.find('textarea').value = '';
